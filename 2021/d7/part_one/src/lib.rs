9b2c09adf66bcc43085800a8e4aaa5ec6423529a
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2021-12-07 10:52:42 +0100

    d7 2021 finished

---
+++ 2021/d7/part_one/src/lib.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+pub fn solve(filename: &str) -> i32 {
+    let input = fs::read_to_string(filename).unwrap();
+
+    let numbers: Vec<i32> = input
+        .split(',')
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+
+    let max = *numbers.iter().max().unwrap();
+    let min = *numbers.iter().min().unwrap();
+
+    let mut sum: i32;
+    let mut lastsum = std::i32::MAX;
+
+    for i in min..=max {
+        sum = 0;
+        for n in &numbers {
+            let diff = i - n;
+            sum += diff.abs();
+        }
+        if sum < lastsum {
+            lastsum = sum;
+        }
+    }
+
+    lastsum
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(solve("input_test.txt"), 37);
+    }
+}
