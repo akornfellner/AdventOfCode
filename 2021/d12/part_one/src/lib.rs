7b60408f2f792159affcfde2c0b04cae99ff59de
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2021-12-12 14:18:54 +0100

    d12 2021 finished

---
+++ 2021/d12/part_one/src/lib.rs
@@ -0,0 +1,81 @@
+use std::{collections::HashMap, fs};
+
+pub fn solve(filename: &str) -> i32 {
+    let input = fs::read_to_string(filename).unwrap();
+
+    let lines: Vec<&str> = input.lines().collect();
+
+    let mut caves: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    for line in lines {
+        let tmp: Vec<&str> = line.split('-').collect();
+        let s = tmp[0];
+        let e = tmp[1];
+
+        if caves.contains_key(s) {
+            let mut n: Vec<&str> = caves[&s].clone();
+            n.push(e);
+            caves.insert(s, n);
+        } else {
+            caves.insert(s, vec![e]);
+        }
+
+        let tmp: Vec<&str> = line.split('-').collect();
+        let e = tmp[0];
+        let s = tmp[1];
+
+        if caves.contains_key(s) {
+            let mut n: Vec<&str> = caves[&s].clone();
+            n.push(e);
+            caves.insert(s, n);
+        } else {
+            caves.insert(s, vec![e]);
+        }
+    }
+
+    let visited: Vec<&str> = vec![];
+
+    let result = visit("start", &caves, &visited);
+
+    result
+}
+
+fn visit(cave: &str, caves: &HashMap<&str, Vec<&str>>, visited: &Vec<&str>) -> i32 {
+    let mut visited = visited.clone();
+    if cave == "end" {
+        return 1;
+    }
+
+    let neighbors = &caves[cave];
+
+    let mut result = 0;
+
+    if is_small(cave) && visited.contains(&cave) {
+        return 0;
+    }
+
+    visited.push(cave);
+
+    for n in neighbors {
+        result += visit(*n, caves, &visited);
+    }
+
+    result
+}
+
+fn is_small(cave: &str) -> bool {
+    if cave == cave.to_lowercase() {
+        return true;
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(solve("input_test.txt"), 10);
+    }
+}
