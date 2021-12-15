a3c3b0e8758621bb08bc5b92b4d61300f5c19e6c
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2021-12-15 22:48:17 +0100

    d15 2021 finished

--- 2021/d15/src/lib.rs
+++ 2021/d15/src/lib.rs
@@ -1,17 +1,17 @@
-use std::fs;
+use pathfinding::prelude::dijkstra;
+use std::{collections::HashMap, fs};
 
-pub fn solve(filename: &str, p2: bool) -> i64 {
+pub fn solve(filename: &str, p2: bool) -> usize {
     let input = fs::read_to_string(filename).unwrap();
 
     let lines: Vec<&str> = input.lines().collect();
 
-    let mut numbers: Vec<Vec<i64>> = vec![];
-    let mut risks: Vec<Vec<i64>> = vec![];
+    let mut numbers: Vec<Vec<usize>> = vec![];
 
     for line in lines {
-        let mut l: Vec<i64> = vec![];
+        let mut l: Vec<usize> = vec![];
         for c in line.chars().collect::<Vec<char>>() {
-            l.push(c as i64 - 48);
+            l.push(c as usize - 48);
         }
         numbers.push(l);
     }
@@ -20,72 +20,68 @@ pub fn solve(filename: &str, p2: bool) -> i64 {
         numbers = expand_field(&numbers);
     }
 
-    for line in &numbers {
-        let mut l: Vec<i64> = vec![];
-        for _ in line {
-            l.push(std::i64::MAX);
-        }
-        risks.push(l);
-    }
-
-    risks[0][0] = 0;
-
-    let mut todo = vec![(0usize, 0usize)];
-
     let rows = numbers.len() - 1;
     let cols = numbers[0].len() - 1;
 
-    while !todo.is_empty() {
-        let (i, j) = todo.pop().unwrap();
-        let value = risks[i][j];
+    let mut risks: HashMap<Pos, usize> = HashMap::new();
 
-        if i > 0 {
-            let (m, n) = (i - 1, j);
-            let old = risks[m][n];
-            let new = numbers[m][n] + value;
-            if new < old {
-                risks[m][n] = new;
-                todo.push((m, n));
-            }
-        }
-        if j > 0 {
-            let (m, n) = (i, j - 1);
-            let old = risks[m][n];
-            let new = numbers[m][n] + value;
-            if new < old {
-                risks[m][n] = new;
-                todo.push((m, n));
-            }
-        }
-        if i < rows {
-            let (m, n) = (i + 1, j);
-            let old = risks[m][n];
-            let new = numbers[m][n] + value;
-            if new < old {
-                risks[m][n] = new;
-                todo.push((m, n));
-            }
-        }
-        if j < cols {
-            let (m, n) = (i, j + 1);
-            let old = risks[m][n];
-            let new = numbers[m][n] + value;
-            if new < old {
-                risks[m][n] = new;
-                todo.push((m, n));
-            }
+    for i in 0..=rows {
+        for j in 0..=cols {
+            risks.insert(Pos(i, j), numbers[i][j]);
         }
     }
 
-    risks[rows][cols]
+    let end = Pos(rows, cols);
+
+    let result = dijkstra(
+        &Pos(0, 0),
+        |p| p.neighbours(&risks, rows, cols),
+        |p| *p == end,
+    );
+
+    result.unwrap().1
 }
 
-pub fn expand_field(numbers: &[Vec<i64>]) -> Vec<Vec<i64>> {
-    let mut expand: Vec<Vec<i64>> = vec![];
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(usize, usize);
+
+impl Pos {
+    fn neighbours(
+        &self,
+        risks: &HashMap<Pos, usize>,
+        rows: usize,
+        cols: usize,
+    ) -> Vec<(Pos, usize)> {
+        let mut result: Vec<(Pos, usize)> = vec![];
+
+        let &Pos(i, j) = self;
+
+        if self.0 > 0 {
+            result.push((Pos(i - 1, j), risks[&Pos(i - 1, j)]));
+        }
+
+        if self.1 > 0 {
+            result.push((Pos(i, j - 1), risks[&Pos(i, j - 1)]));
+        }
+
+        if self.0 < rows {
+            result.push((Pos(i + 1, j), risks[&Pos(i + 1, j)]));
+        }
+
+        if self.1 < cols {
+            result.push((Pos(i, j + 1), risks[&Pos(i, j + 1)]));
+        }
+
+        result
+    }
+}
+
+pub fn expand_field(numbers: &[Vec<usize>]) -> Vec<Vec<usize>> {
+    let mut expand: Vec<Vec<usize>> = vec![];
 
     for line in numbers {
-        let mut l: Vec<i64> = vec![];
-        for i in 0..5i64 {
+        let mut l: Vec<usize> = vec![];
+        for i in 0..5 {
             for number in line {
                 l.push(reduce(number + i));
             }
@@ -96,9 +92,9 @@ pub fn expand_field(numbers: &[Vec<i64>]) -> Vec<Vec<i64>> {
     let tmp = expand.clone();
     expand.clear();
 
-    for i in 0..5i64 {
+    for i in 0..5 {
         for line in &tmp {
-            let new: Vec<i64> = line.iter().map(|x| reduce(*x + i)).collect();
+            let new: Vec<usize> = line.iter().map(|x| reduce(*x + i)).collect();
             expand.push(new);
         }
     }
@@ -106,7 +102,7 @@ pub fn expand_field(numbers: &[Vec<i64>]) -> Vec<Vec<i64>> {
     expand
 }
 
-fn reduce(mut x: i64) -> i64 {
+fn reduce(mut x: usize) -> usize {
     if x > 9 {
         x -= 9;
     }
