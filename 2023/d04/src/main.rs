739144db23c2bdff647be8896efcc48f3eb3e9b4
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2023-12-04 09:52:52 +0100

    2023 d04 finished

---
+++ 2023/d04/src/main.rs
@@ -0,0 +1,76 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
+
+fn main() {
+    let (p1, p2) = solve("input.in");
+    println!("Part one: {}", p1);
+    println!("Part two: {}", p2);
+}
+
+fn solve(filename: &str) -> (u32, u32) {
+    let input = fs::read_to_string(filename).unwrap();
+    let lines: Vec<&str> = input.lines().collect();
+
+    let mut winning: Vec<HashSet<i32>> = vec![];
+    let mut own: Vec<HashSet<i32>> = vec![];
+
+    for line in lines {
+        let parts = line.split(" | ").collect::<Vec<&str>>();
+        let mut winning_set: HashSet<i32> = HashSet::new();
+
+        for number in parts[0].split_whitespace().skip(2) {
+            winning_set.insert(number.parse::<i32>().unwrap());
+        }
+
+        let mut own_set: HashSet<i32> = HashSet::new();
+
+        for number in parts[1].split_whitespace() {
+            own_set.insert(number.parse::<i32>().unwrap());
+        }
+
+        winning.push(winning_set);
+        own.push(own_set);
+    }
+
+    let mut p1 = 0;
+    let mut p2: u32 = 0;
+
+    let mut duplicates: HashMap<usize, u32> = HashMap::new();
+
+    for i in 0..winning.len() {
+        let matches = winning[i].intersection(&own[i]).count() as u32;
+        if matches > 0 {
+            p1 += 2u32.pow(matches - 1);
+        }
+
+        let copies = *duplicates.entry(i).or_insert(1);
+
+        for k in i + 1..=i + matches as usize {
+            let entry = duplicates.entry(k).or_insert(1);
+            *entry += copies;
+        }
+    }
+
+    for i in 0..winning.len() {
+        p2 += duplicates[&i];
+    }
+
+    (p1, p2)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one() {
+        assert_eq!(solve("input_test.in").0, 13);
+    }
+
+    #[test]
+    fn test_two() {
+        assert_eq!(solve("input_test.in").1, 30);
+    }
+}
