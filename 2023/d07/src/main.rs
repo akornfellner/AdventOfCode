4bb241c0b0adee37d944fc499c6d2afd9ef28b12
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2023-12-07 13:07:57 +0100

    d07 improved

--- 2023/d07/src/main.rs
+++ 2023/d07/src/main.rs
@@ -1,20 +1,29 @@
 use std::cmp::Ordering;
 
 fn main() {
-    let p2 = solve("input.in");
+    let (p1, p2) = solve("input.in");
+    println!("Part one: {}", p1);
     println!("Part two: {}", p2);
 }
 
-fn solve(filename: &str) -> i32 {
+fn solve(filename: &str) -> (i32, i32) {
     let input = std::fs::read_to_string(filename).unwrap();
-    let mut hands = input.lines().map(Hand::new).collect::<Vec<Hand>>();
-    let mut result = 0;
+    let mut hands1 = input
+        .lines()
+        .map(|h| Hand::new(h, false))
+        .collect::<Vec<Hand>>();
+    let mut hands2 = input
+        .lines()
+        .map(|h| Hand::new(h, true))
+        .collect::<Vec<Hand>>();
+    let mut result = (0, 0);
 
-    hands.sort();
-    hands.reverse();
+    hands1.sort();
+    hands2.sort();
 
-    for (i, hand) in hands.iter().enumerate() {
-        result += hand.bid * (i as i32 + 1);
+    for (i, hand) in hands1.iter().enumerate() {
+        result.0 += hand.bid * (i as i32 + 1);
+        result.1 += hands2[i].bid * (i as i32 + 1);
     }
 
     result
@@ -22,29 +31,32 @@ fn solve(filename: &str) -> i32 {
 
 #[derive(Debug, PartialEq, Eq)]
 struct Hand {
-    cards: Vec<Card>,
+    cards: Vec<i32>,
     bid: i32,
     typ: Type,
 }
 
 impl Hand {
-    fn new(line: &str) -> Self {
+    fn new(line: &str, two: bool) -> Self {
         let parts = line.split_whitespace().collect::<Vec<&str>>();
         let bid = parts[1].parse::<i32>().unwrap();
-        let cards = parts[0].chars().map(Card::from).collect::<Vec<Card>>();
-        let typ = Hand::get_type(&cards);
+        let cards = parts[0]
+            .chars()
+            .map(|c| Hand::get_card(c, two))
+            .collect::<Vec<i32>>();
+        let typ = Hand::get_type(&cards, two);
 
         Hand { cards, bid, typ }
     }
 
-    fn get_type(cards: &[Card]) -> Type {
+    fn get_type(cards: &[i32], two: bool) -> Type {
         let cards = cards.to_vec();
         let mut counts = [0; 13];
         let mut typ = Type::HighCard;
         let mut j = 0;
 
         for card in cards {
-            if card == Card::J {
+            if card < 0 {
                 j += 1;
             } else {
                 counts[card as usize] += 1;
@@ -68,20 +80,39 @@ impl Hand {
             typ = Type::Pair;
         }
 
-        for _ in 0..j {
-            let typ_new = match typ {
-                Type::Five => Type::Five,
-                Type::Four => Type::Five,
-                Type::FullHouse => Type::Four,
-                Type::Three => Type::Four,
-                Type::TwoPairs => Type::FullHouse,
-                Type::Pair => Type::Three,
-                Type::HighCard => Type::Pair,
-            };
-            typ = typ_new;
+        if two {
+            for _ in 0..j {
+                let typ_new = match typ {
+                    Type::Five => Type::Five,
+                    Type::Four => Type::Five,
+                    Type::FullHouse => Type::Four,
+                    Type::Three => Type::Four,
+                    Type::TwoPairs => Type::FullHouse,
+                    Type::Pair => Type::Three,
+                    Type::HighCard => Type::Pair,
+                };
+                typ = typ_new;
+            }
         }
 
         typ
+    }
+
+    fn get_card(c: char, two: bool) -> i32 {
+        match c {
+            'A' => 12,
+            'K' => 11,
+            'Q' => 10,
+            'J' => {
+                if two {
+                    -1
+                } else {
+                    9
+                }
+            }
+            'T' => 8,
+            _ => c.to_digit(10).unwrap() as i32 - 2,
+        }
     }
 }
 
@@ -108,13 +139,13 @@ impl PartialOrd for Hand {
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum Type {
-    Five,
-    Four,
-    FullHouse,
-    Three,
-    TwoPairs,
-    Pair,
     HighCard,
+    Pair,
+    TwoPairs,
+    Three,
+    FullHouse,
+    Four,
+    Five,
 }
 
 impl Ord for Type {
@@ -131,64 +162,17 @@ impl PartialOrd for Type {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Card {
-    A,
-    K,
-    Q,
-    Ten,
-    Nine,
-    Eight,
-    Seven,
-    Six,
-    Five,
-    Four,
-    Three,
-    Two,
-    J,
-}
-
-impl Card {
-    fn from(c: char) -> Self {
-        match c {
-            'A' => Card::A,
-            'K' => Card::K,
-            'Q' => Card::Q,
-            'J' => Card::J,
-            'T' => Card::Ten,
-            '9' => Card::Nine,
-            '8' => Card::Eight,
-            '7' => Card::Seven,
-            '6' => Card::Six,
-            '5' => Card::Five,
-            '4' => Card::Four,
-            '3' => Card::Three,
-            '2' => Card::Two,
-            _ => panic!("Invalid card"),
-        }
-    }
-}
-
-impl Ord for Card {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let a = *self as i32;
-        let b = *other as i32;
-        a.cmp(&b)
-    }
-}
-
-impl PartialOrd for Card {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn test_one() {
+        assert_eq!(solve("input_test.in").0, 6440);
+    }
+
+    #[test]
     fn test_two() {
-        assert_eq!(solve("input_test.in"), 5905);
+        assert_eq!(solve("input_test.in").1, 5905);
     }
 }
