3cbee929dba358aa0ef3a4388e6fc74c9ea55c76
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2023-12-08 10:04:05 +0100

    2016 d01 finished

--- 2016/d01/src/main.rs
+++ 2016/d01/src/main.rs
@@ -11,8 +11,9 @@ fn solve(filename: &str) -> (i32, i32) {
     let cmds = input.split(", ").map(Cmd::from).collect::<Vec<Cmd>>();
     let mut curr = 0;
     let mut pos = (0, 0);
+    let mut visited: Vec<(i32, i32)> = vec![];
 
-    for cmd in cmds {
+    for cmd in &cmds {
         curr = match cmd.direction {
             Direction::Right => (curr + 1) % 4,
             Direction::Left => (curr + 3) % 4,
@@ -27,9 +28,33 @@ fn solve(filename: &str) -> (i32, i32) {
         }
     }
 
-    println!("{:?}", pos);
-
     result.0 = pos.0.abs() + pos.1.abs();
+
+    pos = (0, 0);
+    curr = 0;
+
+    'outer: for cmd in &cmds {
+        curr = match cmd.direction {
+            Direction::Right => (curr + 1) % 4,
+            Direction::Left => (curr + 3) % 4,
+        };
+        for _ in 0..cmd.value {
+            match curr {
+                0 => pos.0 -= 1,
+                1 => pos.1 -= 1,
+                2 => pos.0 += 1,
+                3 => pos.1 += 1,
+                _ => panic!("Invalid direction"),
+            }
+
+            if visited.contains(&pos) {
+                result.1 = pos.0.abs() + pos.1.abs();
+                break 'outer;
+            } else {
+                visited.push(pos);
+            }
+        }
+    }
 
     result
 }
@@ -47,7 +72,11 @@ impl Cmd {
             'L' => Direction::Left,
             _ => panic!("Invalid direction"),
         };
-        let value = parts[1].to_digit(10).unwrap() as i32;
+        let value = parts[1..]
+            .iter()
+            .collect::<String>()
+            .parse::<i32>()
+            .unwrap();
 
         Cmd { direction, value }
     }
