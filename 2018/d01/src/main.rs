fc7f0da417fc636a6b0431af7e5125d5afda05a9
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2024-12-10 16:03:08 +0100

    2018 d01 finished

---
+++ 2018/d01/src/main.rs
@@ -0,0 +1,32 @@
+use std::collections::HashSet;
+
+use stopwatch::time;
+
+#[time]
+fn main() {
+    let (p1, p2) = solve("input.txt");
+    println!("Part one: {}", p1);
+    println!("Part two: {}", p2);
+}
+
+fn solve(filename: &str) -> (i32, i32) {
+    let input = std::fs::read_to_string(filename).unwrap();
+
+    let numbers: Vec<i32> = input.lines().map(|l| l.parse::<i32>().unwrap()).collect();
+    let p1 = numbers.iter().sum();
+
+    let mut freqs = HashSet::new();
+    let mut p2 = 0;
+
+    'outer: loop {
+        for n in &numbers {
+            p2 += n;
+            if freqs.contains(&p2) {
+                break 'outer;
+            }
+            freqs.insert(p2);
+        }
+    }
+
+    (p1, p2)
+}
