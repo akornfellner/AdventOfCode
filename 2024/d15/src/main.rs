f32d82887fe449571fad2ec1c2bd5cdcdd0e4c1d
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2024-12-15 15:40:24 +0100

    2014 d15 finished

--- 2024/d15/src/main.rs
+++ 2024/d15/src/main.rs
@@ -1,8 +1,8 @@
-use stopwatch::time;
+use std::env::args;
 
-#[time]
 fn main() {
-    let (p1, p2) = solve("input.txt");
+    let filename = args().nth(1).unwrap_or("input_test.txt".to_string());
+    let (p1, p2) = solve(&filename);
     println!("Part one: {}", p1);
     println!("Part two: {}", p2);
 }
@@ -15,126 +15,150 @@ fn solve(filename: &str) -> (usize, usize) {
 
     let parts = input.split("\n\n").collect::<Vec<&str>>();
 
-    let mut field: Vec<Vec<Pos>> = vec![];
-    let mut robot = (0, 0);
+    let mut field1 = vec![];
+    let mut field2 = vec![];
+    let mut robot1 = (0, 0);
 
     for (y, line) in parts[0].lines().enumerate() {
-        let mut row = vec![];
+        let mut row1 = vec![];
+        let mut row2 = vec![];
         for (x, c) in line.chars().enumerate() {
-            let pos = Pos::from_char(c);
-            if let Pos::Robot = pos {
-                robot = (x, y);
+            if c == '@' {
+                robot1 = (x, y);
             }
-            row.push(pos);
+            let add = match c {
+                'O' => ('[', ']'),
+                '@' => ('@', '.'),
+                '#' => ('#', '#'),
+                _ => ('.', '.'),
+            };
+            row1.push(c);
+            row2.push(add.0);
+            row2.push(add.1);
         }
-        field.push(row);
+        field1.push(row1);
+        field2.push(row2);
     }
+
+    let mut robot2 = (robot1.0 * 2, robot1.1);
 
     let moves = parts[1].replace("\n", "");
-    let moves = moves.chars().map(Dir::from_char).collect::<Vec<Dir>>();
+    let moves = moves.chars().collect::<Vec<char>>();
 
     for m in &moves {
-        let (moved, new_pos) = make_move(robot, *m, &mut field);
-        if moved {
-            robot = new_pos;
+        if make_move(robot1, *m, &mut field1) {
+            robot1 = next_pos(robot1, *m);
         }
     }
 
-    (check_sum(&field), 0)
+    for m in &moves {
+        if make_move(robot2, *m, &mut field2) {
+            robot2 = next_pos(robot2, *m);
+        }
+    }
+
+    (checksum(&field1, false), checksum(&field2, true))
 }
 
-fn make_move(pos: (usize, usize), dir: Dir, field: &mut Vec<Vec<Pos>>) -> (bool, (usize, usize)) {
-    let (x, y) = pos;
-    let t = field[y][x];
-    let (dx, dy) = match dir {
-        Dir::Up => (0, -1),
-        Dir::Down => (0, 1),
-        Dir::Left => (-1, 0),
-        Dir::Right => (1, 0),
-    };
+fn make_move((x, y): (usize, usize), dir: char, field: &mut Vec<Vec<char>>) -> bool {
+    let (prev_x, prev_y) = prev_pos((x, y), dir);
 
-    let new_x = (x as i32 + dx) as usize;
-    let new_y = (y as i32 + dy) as usize;
-
-    match field[new_y][new_x] {
-        Pos::Wall => (false, pos),
-        Pos::Box => {
-            let (pushed, _) = make_move((new_x, new_y), dir, field);
-            if pushed {
-                field[y][x] = Pos::Empty;
-                field[new_y][new_x] = t;
-                (true, (new_x, new_y))
+    match field[y][x] {
+        '#' => false,
+        'O' | '[' | ']' => {
+            if field[y][x] == 'O' {
+                if make_move(next_pos((x, y), dir), dir, field) {
+                    field[y][x] = field[prev_y][prev_x];
+                    field[prev_y][prev_x] = '.';
+                    true
+                } else {
+                    false
+                }
             } else {
-                (false, pos)
+                match dir {
+                    '^' | 'v' => {
+                        if is_possible((x, y), dir, field) {
+                            let side = if field[y][x] == '[' { '>' } else { '<' };
+                            let side = next_pos((x, y), side);
+                            make_move(next_pos((x, y), dir), dir, field);
+                            make_move(next_pos(side, dir), dir, field);
+                            field[y][x] = field[prev_y][prev_x];
+                            field[prev_y][prev_x] = '.';
+
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                    _ => {
+                        if make_move(next_pos((x, y), dir), dir, field) {
+                            field[y][x] = field[prev_y][prev_x];
+                            field[prev_y][prev_x] = '.';
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                }
+            }
+        }
+        '@' => {
+            if make_move(next_pos((x, y), dir), dir, field) {
+                field[y][x] = '.';
+                true
+            } else {
+                false
             }
         }
         _ => {
-            field[y][x] = Pos::Empty;
-            field[new_y][new_x] = t;
-            (true, (new_x, new_y))
+            field[y][x] = field[prev_y][prev_x];
+            field[prev_y][prev_x] = '.';
+            true
         }
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Pos {
-    Robot,
-    Wall,
-    Empty,
-    Box,
-}
-
-impl Pos {
-    fn from_char(c: char) -> Self {
-        match c {
-            '#' => Pos::Wall,
-            '.' => Pos::Empty,
-            '@' => Pos::Robot,
-            'O' => Pos::Box,
-            _ => panic!("Unknown character: {}", c),
-        }
+fn next_pos((x, y): (usize, usize), dir: char) -> (usize, usize) {
+    match dir {
+        '^' => (x, y - 1),
+        'v' => (x, y + 1),
+        '<' => (x - 1, y),
+        _ => (x + 1, y),
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Dir {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-impl Dir {
-    fn from_char(c: char) -> Self {
-        match c {
-            '^' => Dir::Up,
-            'v' => Dir::Down,
-            '<' => Dir::Left,
-            '>' => Dir::Right,
-            _ => panic!("Unknown direction: {}", c),
-        }
+fn prev_pos((x, y): (usize, usize), dir: char) -> (usize, usize) {
+    match dir {
+        '^' => (x, y + 1),
+        'v' => (x, y - 1),
+        '<' => (x + 1, y),
+        _ => (x - 1, y),
     }
 }
 
-fn pring_field(field: &[Vec<Pos>]) {
-    for row in field {
-        for pos in row {
-            match pos {
-                Pos::Robot => print!("@"),
-                Pos::Wall => print!("#"),
-                Pos::Empty => print!("."),
-                Pos::Box => print!("O"),
-            }
+fn is_possible((x, y): (usize, usize), dir: char, field: &[Vec<char>]) -> bool {
+    match field[y][x] {
+        '#' => false,
+        '[' => {
+            let next = next_pos((x, y), dir);
+            let right = next_pos(next, '>');
+            is_possible(next, dir, field) && is_possible(right, dir, field)
         }
-        println!();
+        ']' => {
+            let next = next_pos((x, y), dir);
+            let left = next_pos(next, '<');
+            is_possible(next, dir, field) && is_possible(left, dir, field)
+        }
+        _ => true,
     }
 }
 
-fn check_sum(field: &[Vec<Pos>]) -> usize {
+fn checksum(field: &[Vec<char>], two: bool) -> usize {
+    let compare = if two { '[' } else { 'O' };
     let mut result = 0;
     for (y, row) in field.iter().enumerate() {
-        for (x, pos) in row.iter().enumerate() {
-            if let Pos::Box = pos {
+        for (x, &c) in row.iter().enumerate() {
+            if c == compare {
                 result += y * 100 + x;
             }
         }
