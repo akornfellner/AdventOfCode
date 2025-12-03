e7d0b840a45e81c001b728c39717b0bb103ccff4
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2025-12-03 12:51:29 +0100

    2025 d03 improved

--- 2025/d03/src/main.rs
+++ 2025/d03/src/main.rs
@@ -19,32 +19,25 @@ fn solve(filename: &str) -> (usize, usize) {
     let mut p2 = 0;
 
     for line in input.lines() {
-        let mut last_index = 0;
-        let mut sum = 0;
-
-        for level in 0..2 {
-            let level = 2 - level - 1;
-            let (max, new_last_index) = find_max(&line[last_index..], level);
-            sum += max as u64 * (10_u64).pow(level.try_into().unwrap());
-            last_index += new_last_index + 1;
-        }
-
-        p1 += sum as usize;
-
-        let mut sum = 0;
-        let mut last_index = 0;
-
-        for level in 0..12 {
-            let level = 12 - level - 1;
-            let (max, new_last_index) = find_max(&line[last_index..], level);
-            sum += max as u64 * (10_u64).pow(level.try_into().unwrap());
-            last_index += new_last_index + 1;
-        }
-
-        p2 += sum as usize;
+        p1 += get_joltage(line, 2);
+        p2 += get_joltage(line, 12);
     }
 
     (p1, p2)
+}
+
+fn get_joltage(line: &str, number: usize) -> usize {
+    let mut last_index = 0;
+    let mut sum = 0;
+
+    for level in 0..number {
+        let level = number - level - 1;
+        let (max, new_last_index) = find_max(&line[last_index..], level);
+        sum += max as u64 * (10_u64).pow(level.try_into().unwrap());
+        last_index += new_last_index + 1;
+    }
+
+    sum as usize
 }
 
 fn find_max(s: &str, level: usize) -> (usize, usize) {
